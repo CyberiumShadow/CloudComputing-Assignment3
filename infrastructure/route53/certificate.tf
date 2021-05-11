82b46c355a8156d911d98ca80ed37bcb2fced320
Johnson Chen <cyberiumdev@gmail.com>
2021-05-12 01:59:51 +1000

    terraform: add wildcard SAN to cert

--- infrastructure/route53/certificate.tf
+++ infrastructure/route53/certificate.tf
@@ -1,17 +1,18 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "neocar.link"
-  validation_method = "DNS"
+  domain_name               = "neocar.link"
+  subject_alternative_names = ["*.neocar.link"]
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-output "acmCert" {
-  value = aws_acm_certificate.cert.arn
-}
-
-resource "aws_acm_certificate_validation" "example" {
+resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.acm : record.fqdn]
+}
+
+output "acmCert" {
+  value = aws_acm_certificate.cert.arn
 }
