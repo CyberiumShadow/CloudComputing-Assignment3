d7bbba89957e90de6036d055f3cbeb0be461f837
Johnson Chen <cyberiumdev@gmail.com>
2021-05-13 00:23:45 +1000

    terraform: add DynamoDB Table

---
+++ infrastructure/dynamodb/db.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "neocar_users" {
+  name = "neocar_users"
+  billing_mode = "PROVISIONED"
+  write_capacity = 5
+  read_capacity = 5
+
+  hash_key = "username"
+  attribute {
+    name = "username"
+    type = "S"
+  }
+
+  tags = {
+    "Name" = "Neocar Users"
+  }
+}
