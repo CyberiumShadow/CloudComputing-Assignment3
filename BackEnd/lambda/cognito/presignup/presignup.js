0a92ba83f530b8190af661c6663167a1a772dd71
Johnson Chen <cyberiumdev@gmail.com>
2021-05-15 16:55:57 +1000

    add presignup Cognito Lambda Trigger

---
+++ BackEnd/lambda/cognito/presignup/presignup.js
@@ -0,0 +1,29 @@
+const { DynamoDBClient, GetItemCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+
+const dbClient = new DynamoDBClient({
+  region: "us-east-1",
+});
+
+exports.handler = async (event, context, callback) => {
+  // Set the user pool autoConfirmUser flag after validating the email domain
+  event.response.autoConfirmUser = true;
+  const {userName, request: {userAttributes, validationData}} = event;
+  const params = {
+    TableName: "neocar_users",
+    Key: {
+      username: { S: userName },
+    },
+  };
+  const { Item } = await dbClient.send(new GetItemCommand(params));
+  if (!Item) {
+    const newUserParams = {
+      TableName: "neocar_users",
+      Item: {
+        username: { S: userName },
+      },
+    };
+    await dbClient.send(new PutItemCommand(newUserParams));
+  }
+  // Return to Amazon Cognito
+  callback(null, event);
+};
