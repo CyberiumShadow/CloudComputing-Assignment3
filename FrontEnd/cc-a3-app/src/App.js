6e551707a22e277b4777e48eb9039bc292b88149
Johnson Chen <cyberiumdev@gmail.com>
2021-05-19 22:06:07 +1000

    remove redundant code and directly retrieve username/accessToken from Amplify

--- FrontEnd/cc-a3-app/src/App.js
+++ FrontEnd/cc-a3-app/src/App.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import { AppContext } from "libs/context";
 import Auth from "@aws-amplify/auth";
-import config from 'config';
 
 // Auth components
-import Login from 'components/auth/login';
-import SignUp from 'components/auth/signUp';
+import Login from "components/auth/login";
+import SignUp from "components/auth/signUp";
 
 // Auth routes
-import AuthenticatedRoute from 'components/auth/authenticatedRoute';
-import UnauthenticatedRoute from 'components/auth/unauthenticatedRoute';
+import AuthenticatedRoute from "components/auth/authenticatedRoute";
+import UnauthenticatedRoute from "components/auth/unauthenticatedRoute";
 
 // Main components
-import Dashboard from 'components/main/dashboard';
-import Profile from 'components/main/profile';
-import HireCar from 'components/main/hireCar';
-import ListCar from 'components/main/listCar';
-import Stats from 'components/main/stats';
+import Dashboard from "components/main/dashboard";
+import Profile from "components/main/profile";
+import HireCar from "components/main/hireCar";
+import ListCar from "components/main/listCar";
+import Stats from "components/main/stats";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [authentication, setAuthentication] = useState({
     isAuthenticated: false,
-    username: '',
-    accessToken: '',
+    username: "",
+    accessToken: "",
   });
 
   useEffect(() => {
@@ -32,36 +31,47 @@ function App() {
   }, []);
 
   const onLoad = async () => {
+    console.log(await Auth.currentAuthenticatedUser());
     try {
-      await Auth.currentSession();
-      const username = localStorage.getItem('CognitoIdentityServiceProvider.' + config.cognito.APP_CLIENT_ID + '.LastAuthUser');
-      const accessToken = localStorage.getItem('CognitoIdentityServiceProvider.' + config.cognito.APP_CLIENT_ID + '.' + username + '.accessToken');
       setAuthentication({
         isAuthenticated: true,
-        username: username,
-        accessToken: accessToken,
+        username: (await Auth.currentUserInfo()).username,
+        accessToken: (await Auth.currentSession()).accessToken,
       });
-    }
-    catch(e) {
-      if (e !== 'No current user') {
-        console.log(e)
+    } catch (e) {
+      if (e !== "No current user") {
+        console.log(e);
       }
     }
     setIsAuthenticating(false);
-  }
+  };
 
   return (
     !isAuthenticating && (
       <AppContext.Provider value={{ authentication, setAuthentication }}>
         <Switch>
-          <Route exact path="/"><Login /></Route>
-          <UnauthenticatedRoute exact path="/signup"><SignUp /></UnauthenticatedRoute>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <UnauthenticatedRoute exact path="/signup">
+            <SignUp />
+          </UnauthenticatedRoute>
 
-          <AuthenticatedRoute exact path="/dashboard"><Dashboard /></AuthenticatedRoute>
-          <AuthenticatedRoute exact path="/profile"><Profile /></AuthenticatedRoute>
-          <AuthenticatedRoute exact path="/hire-car"><HireCar /></AuthenticatedRoute>
-          <AuthenticatedRoute exact path="/list-car"><ListCar /></AuthenticatedRoute>
-          <AuthenticatedRoute exact path="/stats"><Stats /></AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/dashboard">
+            <Dashboard />
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/profile">
+            <Profile />
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/hire-car">
+            <HireCar />
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/list-car">
+            <ListCar />
+          </AuthenticatedRoute>
+          <AuthenticatedRoute exact path="/stats">
+            <Stats />
+          </AuthenticatedRoute>
         </Switch>
       </AppContext.Provider>
     )
--- FrontEnd/cc-a3-app/src/components/auth/login.js
+++ FrontEnd/cc-a3-app/src/components/auth/login.js
@@ -1,79 +1,106 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import Auth from '@aws-amplify/auth';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Auth from "@aws-amplify/auth";
 import { useAppContext } from "libs/context";
-import Graphics from 'components/utils/graphics';
-import LoadingButton from 'components/utils/loadingButton';
-import styles from './auth.module.css';
-import config from 'config';
+import Graphics from "components/utils/graphics";
+import LoadingButton from "components/utils/loadingButton";
+import styles from "./auth.module.css";
 
 function Login() {
   const history = useHistory();
   const { setAuthentication } = useAppContext();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [form, setForm] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     try {
       await Auth.signIn(form.username, form.password);
-      const username = localStorage.getItem('CognitoIdentityServiceProvider.' + config.cognito.APP_CLIENT_ID + '.LastAuthUser');
-      const accessToken = localStorage.getItem('CognitoIdentityServiceProvider.' + config.cognito.APP_CLIENT_ID + '.' + username + '.accessToken');
       setAuthentication({
         isAuthenticated: true,
-        username: username,
-        accessToken: accessToken,
+        username: (await Auth.currentUserInfo()).username,
+        accessToken: (await Auth.currentSession()).accessToken,
       });
-      history.push('/dashboard');
+      history.push("/dashboard");
     } catch (error) {
-      console.log('error signing in', error);
+      console.log("error signing in", error);
       let errorMsg = error.message;
-      setError((errorMsg.charAt(errorMsg.length-1) === '.') ? errorMsg.slice(0, -1) : errorMsg);
+      setError(
+        errorMsg.charAt(errorMsg.length - 1) === "."
+          ? errorMsg.slice(0, -1)
+          : errorMsg
+      );
       setIsLoading(false);
     }
-  }
+  };
 
   return (
     <div>
-      <Graphics/>
+      <Graphics />
       <div className={styles.formWrapper}>
         <form onSubmit={handleSubmit}>
           <div className="form-group mb-4">
             <h3 className={styles.formTitle}>Welcome!</h3>
           </div>
-          {
-            error.length > 0 &&
-              <div className={`form-group mb-3 ${styles.formError}`}>
-                <div>{error}</div>
-                <div>Please try again</div>
-              </div>
-          }
+          {error.length > 0 && (
+            <div className={`form-group mb-3 ${styles.formError}`}>
+              <div>{error}</div>
+              <div>Please try again</div>
+            </div>
+          )}
           <div className="form-group mb-3">
-            <label>Username</label><br />
-            <input type="text" className={styles.inputBody} placeholder="enter username" spellCheck={false} required={true} name="username" value={form.username} onChange={handleChange} />
+            <label>Username</label>
+            <br />
+            <input
+              type="text"
+              className={styles.inputBody}
+              placeholder="enter username"
+              spellCheck={false}
+              required={true}
+              name="username"
+              value={form.username}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group mb-4">
-            <label>Password</label><br />
-            <input type="password" className={styles.inputBody} placeholder="enter password" spellCheck={false} required={true} name="password" value={form.password} onChange={handleChange} />
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              className={styles.inputBody}
+              placeholder="enter password"
+              spellCheck={false}
+              required={true}
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group mb-3">
-            <LoadingButton isLoading={isLoading} text={'Login'} disabled={false}/>
+            <LoadingButton
+              isLoading={isLoading}
+              text={"Login"}
+              disabled={false}
+            />
           </div>
           <div className="form-group mb-3">
             <small>
-              Don't have an account? <Link className="link" to={"/signup"}>Sign Up</Link>
+              Don't have an account?{" "}
+              <Link className="link" to={"/signup"}>
+                Sign Up
+              </Link>
             </small>
           </div>
         </form>
