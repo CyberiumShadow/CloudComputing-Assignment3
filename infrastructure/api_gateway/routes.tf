47f3789ff3e8ea477321a702a673ec249fe31547
Johnson Chen <cyberiumdev@gmail.com>
2021-06-08 03:13:01 +1000

    add api routes

--- infrastructure/api_gateway/routes.tf
+++ infrastructure/api_gateway/routes.tf
@@ -1,6 +1,30 @@
-resource "aws_apigatewayv2_route" "name" {
+resource "aws_apigatewayv2_route" "root" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "GET /"
+  authorization_type = "NONE"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
+resource "aws_apigatewayv2_route" "getCars" {
   api_id             = aws_apigatewayv2_api.neocar_api.id
   route_key          = "GET /cars"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
+resource "aws_apigatewayv2_route" "postCar" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "POST /cars"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
+resource "aws_apigatewayv2_route" "postCarBookings" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "POST /cars/{carid}/bookings"
   authorization_type = "JWT"
   authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
   target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
