2e14fc9c72bd7709e58d72822016a052759dce29
Johnson Chen <cyberiumdev@gmail.com>
2021-06-12 19:55:24 +1000

    PATCH/DELETE car, DELETE booking

--- BackEnd/app/src/routes/cars.js
+++ BackEnd/app/src/routes/cars.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-const { GetItemCommand, PutItemCommand, ScanCommand } = require('@aws-sdk/client-dynamodb');
+const {
+  GetItemCommand,
+  PutItemCommand,
+  ScanCommand,
+  DeleteItemCommand,
+  QueryCommand,
+  BatchWriteItemCommand,
+  UpdateItemCommand,
+} = require('@aws-sdk/client-dynamodb');
 const { PutObjectCommand } = require('@aws-sdk/client-s3');
 const { nanoid } = require('nanoid');
 const { dbClient, s3Client } = require('../lib/aws');
@@ -29,7 +37,6 @@ router.get('/', async (req, res) => {
   const carParams = {
     TableName: 'cars',
   };
-  // const { Items } = await dbClient.send(new ScanCommand(bookingParams));
   const { Items } = await dbClient.send(new ScanCommand(carParams));
   if (Items.length === 0)
     return res.status(200).json({ message: 'No result is retrieved. Please query again' });
@@ -66,9 +73,21 @@ router.post('/', uploader.single('image'), async (req, res) => {
           price: { N: price },
           minHour: { N: minHour },
           address: { S: address },
+          image: { S: `https://${uploadParams.Bucket}/${uploadParams.Key}` },
         },
       };
+      const updateUserParams = {
+        TableName: 'users',
+        Key: {
+          username: { S: owner },
+        },
+        ExpressionAttributeValues: {
+          ':plate': { S: plate },
+        },
+        UpdateExpression: 'SET currentListing = :plate',
+      };
       await dbClient.send(new PutItemCommand(newCarParams));
+      await dbClient.send(new UpdateItemCommand(updateUserParams));
       await s3Client.send(new PutObjectCommand(uploadParams));
       return res.status(200).send({ message: 'Car Listed' });
     }
@@ -76,6 +95,94 @@ router.post('/', uploader.single('image'), async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ err });
+  }
+});
+
+router.patch('/:carid', async (req, res) => {
+  const {
+    params: { carid },
+    body: { price, minHour, address },
+  } = req;
+  const params = {
+    TableName: 'cars',
+    Key: {
+      licence_plate: { S: carid },
+    },
+  };
+  try {
+    const { Item } = await dbClient.send(new GetItemCommand(params));
+    console.log(Item);
+    if (Item) {
+      const patchCarParams = {
+        TableName: 'cars',
+        Key: {
+          licence_plate: { S: carid },
+        },
+        ExpressionAttributeNames: {
+          '#price': 'price',
+          '#minHour': 'minHour',
+          '#address': 'address',
+        },
+        ExpressionAttributeValues: {
+          ':price': { N: `${price}` },
+          ':minHour': { N: `${minHour}` },
+          ':address': { S: address },
+        },
+        UpdateExpression: 'SET #price = :price, #minHour = :minHour, #address = :address',
+      };
+      await dbClient.send(new UpdateItemCommand(patchCarParams));
+      return res.status(200).send({ message: `${carid} updated` });
+    }
+    return res.status(409).json({ message: `${carid} does not exist` });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ err });
+  }
+});
+
+router.delete('/:carid', async (req, res) => {
+  const {
+    params: { carid },
+  } = req;
+  const deleteCarParams = {
+    TableName: 'cars',
+    Key: {
+      licence_plate: { S: carid },
+    },
+  };
+  const queryBookingParams = {
+    TableName: 'bookings',
+    KeyConditionExpression: '#carid = :carid',
+    ExpressionAttributeNames: {
+      '#carid': 'licence_plate',
+    },
+    ExpressionAttributeValues: {
+      ':carid': { S: carid },
+    },
+  };
+  try {
+    const { Items } = await dbClient.send(new QueryCommand(queryBookingParams));
+    if (Items.length > 0) {
+      const batchItems = Items.map((booking) => ({
+        DeleteRequest: {
+          Key: {
+            licence_plate: booking.licence_plate,
+            booking_id: booking.booking_id,
+          },
+        },
+      }));
+      const batchDeleteParams = {
+        RequestItems: {
+          bookings: batchItems,
+        },
+      };
+      await dbClient.send(new BatchWriteItemCommand(batchDeleteParams));
+    }
+    await dbClient.send(new DeleteItemCommand(deleteCarParams));
+    return res.status(200).json({ message: `${carid} deleted` });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
   }
 });
 
@@ -93,12 +200,43 @@ router.post('/:carid/bookings', async (req, res) => {
       cost: { N: `${body.cost}` },
     },
   };
+  const updateUserParams = {
+    TableName: 'users',
+    Key: {
+      username: { S: body.customer },
+    },
+    ExpressionAttributeValues: {
+      ':booking': { S: newID },
+    },
+    UpdateExpression: 'SET currentBooking = :booking',
+  };
   try {
     await dbClient.send(new PutItemCommand(newBookingParams));
+    await dbClient.send(new UpdateItemCommand(updateUserParams));
     return res.status(200).json({ booking_id: newID });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: err });
+    return res.status(500).json({ err });
+  }
+});
+
+router.delete('/:carid/bookings/:bookingid', async (req, res) => {
+  const {
+    params: { carid, bookingid },
+  } = req;
+  const deleteBookingParams = {
+    TableName: 'bookings',
+    Key: {
+      licence_plate: { S: carid },
+      booking_id: { S: bookingid },
+    },
+  };
+  try {
+    await dbClient.send(new DeleteItemCommand(deleteBookingParams));
+    return res.status(200).json({ message: `${bookingid} deleted` });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
   }
 });
 
--- infrastructure/api_gateway/routes.tf
+++ infrastructure/api_gateway/routes.tf
@@ -21,9 +21,33 @@ resource "aws_apigatewayv2_route" "postCar" {
   target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
 }
 
+resource "aws_apigatewayv2_route" "editCar" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "PATCH /cars/{carid}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
+resource "aws_apigatewayv2_route" "deleteCar" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "DELETE /cars/{carid}"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
 resource "aws_apigatewayv2_route" "postCarBookings" {
   api_id             = aws_apigatewayv2_api.neocar_api.id
   route_key          = "POST /cars/{carid}/bookings"
+  authorization_type = "JWT"
+  authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
+  target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
+}
+
+resource "aws_apigatewayv2_route" "deleteCarBooking" {
+  api_id             = aws_apigatewayv2_api.neocar_api.id
+  route_key          = "DELETE /cars/{carid}/bookings/{bookingid}"
   authorization_type = "JWT"
   authorizer_id      = aws_apigatewayv2_authorizer.JWT.id
   target             = "integrations/${aws_apigatewayv2_integration.cars.id}"
