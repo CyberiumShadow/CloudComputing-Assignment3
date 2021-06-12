d5be0fc2971865d5c86f7d80b774702479a88258
Johnson Chen <cyberiumdev@gmail.com>
2021-06-12 19:20:32 +1000

    update lambda function

--- BackEnd/lambda/cognito/postconfirm/postconfirm.js
+++ BackEnd/lambda/cognito/postconfirm/postconfirm.js
@@ -1,13 +1,20 @@
-const { DynamoDBClient, GetItemCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+} = require("@aws-sdk/client-dynamodb");
 
 const dbClient = new DynamoDBClient({
   region: "us-east-1",
 });
 
 exports.handler = async (event, context, callback) => {
-  const {userName, request: {userAttributes, validationData}} = event;
+  const {
+    userName,
+    request: { userAttributes, validationData },
+  } = event;
   const params = {
-    TableName: "neocar_users",
+    TableName: "users",
     Key: {
       username: { S: userName },
     },
@@ -15,7 +22,7 @@ exports.handler = async (event, context, callback) => {
   const { Item } = await dbClient.send(new GetItemCommand(params));
   if (!Item) {
     const newUserParams = {
-      TableName: "neocar_users",
+      TableName: "users",
       Item: {
         username: { S: userName },
       },
