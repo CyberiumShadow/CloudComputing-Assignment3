671ea60fe5be8df1fc8a25506bb049a2d1ceaf2b
Johnson Chen <cyberiumdev@gmail.com>
2021-06-13 17:29:36 +1000

    remove time conversion code

--- terraform/environment/dynamodb/db.tf
+++ terraform/environment/dynamodb/db.tf
@@ -1,20 +1,3 @@
-resource "aws_dynamodb_table" "users" {
-  name           = "users"
-  billing_mode   = "PROVISIONED"
-  write_capacity = 5
-  read_capacity  = 5
-
-  hash_key = "username"
-  attribute {
-    name = "username"
-    type = "S"
-  }
-
-  tags = {
-    "Name" = "Neocar Users"
-  }
-}
-
 resource "aws_dynamodb_table" "cars" {
   name           = "cars"
   billing_mode   = "PROVISIONED"
